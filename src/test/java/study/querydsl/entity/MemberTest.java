dd6c64aa23ec10a05e5ce3ec61314c164ffcf553
Dowoon <woodw920@gmail.com>
2022-12-05 13:39:49 +0900

    JPQL vs QueryDsl 시작

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -25,10 +25,10 @@ class MemberTest {
         em.persist(teamB);
 
         Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member1", 20, teamA);
+        Member member2 = new Member("member2", 20, teamA);
 
-        Member member3 = new Member("member1", 30, teamB);
-        Member member4 = new Member("member1", 40, teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
