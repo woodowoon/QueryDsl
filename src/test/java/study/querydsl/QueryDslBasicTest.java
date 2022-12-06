25934abd62dafbd4a19f25162d4e64aa850ff073
Dowoon <woodw920@gmail.com>
2022-12-06 21:36:42 +0900

    조인 -on절

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -238,7 +238,7 @@ public class QueryDslBasicTest {
      * 세타 조인 (막 조인하는 것)
      *  외부 조인이 불가능 하다.
      *  -> 다음에 설명할 조인 on을 사용하면 외부 조인이 가능해졌다.
-     * 회원의 이름이 팀 이름과 같은 회원 조
+     * 회원의 이름이 팀 이름과 같은 회원 조인
      */
     @Test
     public void theta_join() {
@@ -256,4 +256,48 @@ public class QueryDslBasicTest {
                 .containsExactly("teamA", "teamB");
 
     }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    // 내부 조인의 경우 where 을 활용하고 외부조인인 경우 left join 을 사용한다.
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team)
+                // .leftJoin(member.team, team)
+                // .on(team.name.eq("teamA"))
+                .where(team.name.eq("teamA")) // on 절과 결과가 똑같다.
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원 이름이 팀이름과 같은 대상을 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+    }
+
 }
